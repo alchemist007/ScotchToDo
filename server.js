e0f74a4c03b3a94d740eb35392b597e7990a2fe7
nesh <hsenesh@gmail.com>
2016-10-20 15:57:14 -0400

    locally working

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var methodOverride = require('method-override');
 
 //configuration
 //mongoose.connect('mongodb://hsenesh:neshneha@jello.modulusmongo.net:27017/iv4Erari');     // connect to mongoDB database on modulus.io
-mongoose.connect('mongodb://nesh:neshneha@ec2-54-158-172-168.compute-1.amazonaws.com:27017/dummyDB');
 //mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu
 
 //mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
